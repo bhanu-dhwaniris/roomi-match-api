11bd7d86da6b527a920767de2d6da74bf8af212a
bhanu-dhwaniris <bhanu.prakash@dhwaniris.com>
2025-03-19 00:13:10 +0530

    Add route to remove skipped matches for a user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,5 +8,6 @@ router.post('/connections/:userId', auth, userController.sendRequest);
 router.put('/connections/:userId', auth, userController.respondToRequest);
 router.get('/connections', auth, userController.getConnections);
 router.delete('/connections/:userId', auth, userController.removeConnection);
+router.delete('/skipped-matches/:userId', auth, userController.removeMatch);
 
 module.exports = router; 
